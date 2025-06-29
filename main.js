31382afc22cef091bc5b74b08cdf3977117ec09f
gabriel_saucedo <saucedogabriel99@gmail.com>
2025-06-29 19:53:30 -0300

    Starting project

---
+++ main.js
@@ -0,0 +1,10 @@
+// 1. Calcular el área de un rectángulo
+function calcularAreaRectangulo(largo, ancho) {
+  return largo * ancho;
+}
+
+console.log("Solución 1: Área de Rectángulo");
+console.log(calcularAreaRectangulo(5, 3)); // Resultado: 15
+console.log(calcularAreaRectangulo(10, 2)); // Resultado: 20
+console.log(calcularAreaRectangulo(7, 4)); // Resultado: 28
+console.log("-----------------------------");
